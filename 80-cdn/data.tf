2b15fcec567e9f15c23468be0ec50cf9abf7a00f
soumyareddy <soumyareddy816@gmail.com>
2025-04-07 19:07:13 -0400

    expense-infra

--- 80-cdn/data.tf
+++ 80-cdn/data.tf
@@ -5,5 +5,5 @@ data "aws_cloudfront_cache_policy" "cacheEnable" {
   name = "Managed-CachingOptimized"
 }
 data "aws_ssm_parameter" "https_certificate_arn" {  #https cetificate arn , web_alb_certificate arn both are same
-  name = "/${var.project_name}/${var.environment}/https_certificate_arn" # the above name https_certificate_arn , this web_alb_certificate_arn both may have different names also.for understanding purpose evrytime we are giving same names
+  name = "/${var.project_name}/${var.environment}/https_certificate_arn"          # the above name https_certificate_arn , this web_alb_certificate_arn both may have different names also.for understanding purpose evrytime we are giving same names
 }
